c0b4529954839660e97522d51326fd20e899fa61
Rubens <devbsb.rubens@gmail.com>
2024-08-09 16:08:24 -0300

    Anotações JPA para classes de domínio

--- src/main/java/io/github/Hazarinn/domain/Cliente.java
+++ src/main/java/io/github/Hazarinn/domain/Cliente.java
@@ -1,18 +1,31 @@
 package io.github.Hazarinn.domain;
 
+import io.github.Hazarinn.domain.enums.Perfil;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente extends Pessoa{
+
+@Entity
+public class Cliente extends Pessoa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
 
+    @ManyToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Cliente() {
+        super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
