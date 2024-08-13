ee02042c567f3bdcd598801f68885d70703042bb
Rubens <devbsb.rubens@gmail.com>
2024-08-13 13:13:23 -0300

    Create para Chamado

--- src/main/java/io/github/Hazarinn/services/ChamadoService.java
+++ src/main/java/io/github/Hazarinn/services/ChamadoService.java
@@ -1,6 +1,11 @@
 package io.github.Hazarinn.services;
 
 import io.github.Hazarinn.domain.Chamado;
+import io.github.Hazarinn.domain.Cliente;
+import io.github.Hazarinn.domain.Tecnico;
+import io.github.Hazarinn.domain.dtos.ChamadoDTO;
+import io.github.Hazarinn.domain.enums.Prioridade;
+import io.github.Hazarinn.domain.enums.Status;
 import io.github.Hazarinn.repositories.ChamadoRepository;
 import io.github.Hazarinn.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +20,12 @@ public class ChamadoService {
     @Autowired
     private ChamadoRepository repository;
 
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @Autowired
+    private ClienteService clienteService;
+
     public Chamado findById(Integer id) {
         Optional<Chamado> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado" + id));
@@ -22,5 +33,30 @@ public class ChamadoService {
 
     public List<Chamado> findAll() {
         return repository.findAll();
+    }
+
+    public Chamado create(ChamadoDTO objDTO) {
+        return repository.save(newChamado(objDTO));
+
+    }
+
+    private Chamado newChamado(ChamadoDTO obj) {
+
+        Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+        Cliente cliente = clienteService.findById(obj.getCliente());
+        Chamado chamado = new Chamado();
+        if (obj.getId() != null){
+            chamado.setId(obj.getId());
+        }
+
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+        chamado.setStatus(Status.toEnum(obj.getStatus()));
+        chamado.setTitulo(obj.getTitulo());
+        chamado.setObservacoes(obj.getObservacoes());
+        return chamado;
+
+
     }
 }
