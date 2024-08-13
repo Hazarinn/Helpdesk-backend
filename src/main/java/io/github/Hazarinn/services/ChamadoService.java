ae2d1b206e9ea8cad7ca10a7818c83e3dec0b794
Rubens <devbsb.rubens@gmail.com>
2024-08-13 12:33:29 -0300

    FindById para chamados

---
+++ src/main/java/io/github/Hazarinn/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package io.github.Hazarinn.services;
+
+import io.github.Hazarinn.domain.Chamado;
+import io.github.Hazarinn.repositories.ChamadoRepository;
+import io.github.Hazarinn.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado" + id));
+    }
+}
