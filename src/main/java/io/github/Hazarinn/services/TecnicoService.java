7c89c1d6f3663346b58225f688f6f4d8a383e964
Rubens <devbsb.rubens@gmail.com>
2024-08-12 22:56:50 -0300

    Endpont findAll para tecnico

--- src/main/java/io/github/Hazarinn/services/TecnicoService.java
+++ src/main/java/io/github/Hazarinn/services/TecnicoService.java
@@ -3,9 +3,11 @@ package io.github.Hazarinn.services;
 
 import io.github.Hazarinn.domain.Tecnico;
 import io.github.Hazarinn.repositories.TecnicoRepository;
+import io.github.Hazarinn.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,7 +18,12 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: " + id));
 
+    }
+
+    public List<Tecnico> findAll() {
+
+        return repository.findAll();
     }
 }
